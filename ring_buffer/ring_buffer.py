adbb6c98f2bfc81761863d13e175bf6f1e3e65ab
Bryan Szendel <bryan.szendel@gmail.com>
2019-12-20 11:14:35 -0700

    ring buffer adds items to a queue and tracks earliest added item to be replaced

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,20 +1,39 @@
 from doubly_linked_list import DoublyLinkedList
-
+from queue import Queue
 
 class RingBuffer:
     def __init__(self, capacity):
         self.capacity = capacity
-        self.current = None
+        self.size = 0
+        self.queue = Queue()
         self.storage = DoublyLinkedList()
 
     def append(self, item):
-        pass
+        if self.size == self.capacity:
+            popped = self.queue.dequeue() # get val out of queue
+            print('popped', popped)
+            value = self.storage.head
+            print('value', value.value)
+            while value is not None:
+                if value.value == popped:
+                    value.value = item
+                value = value.next
+            self.queue.enqueue(item)
+        if self.size < self.capacity:
+            self.size += 1
+            self.queue.enqueue(item)
+            return self.storage.add_to_tail(item)
+            
 
     def get(self):
         # Note:  This is the only [] allowed
         list_buffer_contents = []
 
         # TODO: Your code here
+        node = self.storage.head
+        while node is not None:
+            list_buffer_contents.append(node.value)
+            node = node.next
 
         return list_buffer_contents
 
